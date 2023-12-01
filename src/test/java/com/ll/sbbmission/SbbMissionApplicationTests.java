bbd94fad923f25c8aa9fefacd004cc714f1c2b9f
lapishi <lapish1214@gmail.com>
2023-12-01 19:09:40 +0900

    2-5

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ll.sbbmission;
+/*package com.ll.sbbmission;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class SbbMissionApplicationTests {
     }
 
 }
+*/
